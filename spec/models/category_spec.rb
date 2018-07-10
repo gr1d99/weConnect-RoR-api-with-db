8c39473841b7c396cc3f542efb297d7687f245a2
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-10 14:43:59 +0300

    feat(categoryModel): create (#1)

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  context 'database columns' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:id).of_type(:integer) }
+  end
+
+  context 'objects creation' do
+    context 'when name attribute is supplied' do
+      it 'is valid' do
+        expect(build(:category)).to be_valid
+      end
+
+      it 'creates a record in database' do
+        create(:category)
+        expect(Category.count).to eq(1)
+      end
+    end
+
+    context 'when name is not supplied' do
+      let(:category) { build(:category, name: '') }
+      let(:errors) { category.errors[:name] }
+
+      it 'is not valid' do
+        expect(category).not_to be_valid
+      end
+
+      it 'will add errors for attribute name' do
+        expect(errors).not_to be_nil
+      end
+    end
+  end
+end
