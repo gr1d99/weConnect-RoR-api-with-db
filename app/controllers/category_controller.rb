1ac3f6aa5f56cfe76a6bad0b6de89c6c8290fd72
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-23 09:02:05 +0300

    feat(updateCategory): add functionality

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,9 +5,26 @@ class CategoryController < ApplicationController
     render json: response, status: :created
   end
 
+  def update
+    id = update_params[:id]
+    category = find_category(id)
+    category.update(update_params)
+    response = { category: category, message: Message.category_updated }
+
+    render json: response, status: :ok
+  end
+
   private
 
   def category_params
     params.permit(:name)
+  end
+
+  def update_params
+    params.permit(:name, :id)
+  end
+
+  def find_category(id)
+    Category.find(id)
   end
 end
