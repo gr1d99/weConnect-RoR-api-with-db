d5edfe492376f5d590982999a4e488ef341f0c01
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-26 18:31:41 +0300

    feate(locationController): add update action (#12)

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -5,9 +5,26 @@ class LocationController < ApplicationController
     render json: response, status: :created
   end
 
+  def update
+    address = params[:address]
+    response = if location_object.address.eql?(address) || address.blank?
+                 { message: Message.location_not_changed }
+               else
+                 location_object.update(location_params)
+                 { message: Message.location_updated, location: location_object }
+               end
+
+    render json: response, status: 200
+  end
+
+
   private
 
   def location_params
     params.permit(:address)
+  end
+
+  def location_object
+    Location.find(params[:id])
   end
 end
