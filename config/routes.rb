11d264edb179cb3c139293195d004b77b5bcd561
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-31 13:13:22 +0300

    chore(categoryController): rename controller (#22)
    - as per REST API best practices it is amust that we
      pluralize routes nouns instead of having them in
      singular form.
    
    why: this commit ensures that all routes associated
         to category controller adhere to the rule
         in context.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/login', to: 'authentication#login'
   post '/register', to: 'authentication#register'
-  resources :category
+
   resources :locations
+  resources :categories
   resources :businesses
 end
--- spec/controllers/category/add_spec.rb
+++ spec/controllers/category/add_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe CategoryController, type: :controller do
+RSpec.describe CategoriesController, type: :controller do
   let(:json) { JSON.parse(response.body) }
 
   describe '#POST .create' do
